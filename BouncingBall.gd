d80d29425799e36938ca0f1a08aa656309305779
Charles <29605680+charliecharlieO-o@users.noreply.github.com>
2022-01-19 10:32:18 -0600

    add acceleration

--- BouncingBall.gd
+++ BouncingBall.gd
@@ -6,7 +6,8 @@ onready var window = Vector2(
 )
 
 var ball: Vector2
-var speed := Vector2(1.0, 3.3)
+var velocity := Vector2(1.0, 3.3)
+var topspeed:float = 10.0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -16,12 +17,17 @@ func _ready():
 
 
 func bounce():
-	# You can add vectors directly with GDscript
-	ball = ball + speed
+	# Apply bouncing logic
 	if ((ball.x > window.x) || (ball.x < 1)):
-		speed.x = speed.x * -1.0
+		velocity.x = velocity.x * -1.0
 	if ((ball.y > window.y) || (ball.y < 1)):
-		speed.y = speed.y * -1.0
+		velocity.y = velocity.y * -1.0
+	# acceleration increases in the same direction
+	var acceleration: Vector2 = velocity.normalized()
+	# You can add vectors directly with GDscript
+	velocity = velocity + velocity.normalized()
+	velocity = velocity.clamped(topspeed) # limit velocity max 10
+	ball = ball + velocity
 
 func _process(delta):
 	update()
